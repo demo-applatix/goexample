3dc68790eaa4804c0c75f3aa5afc89f73ac9ff50
Hong Wang <hong@applatix.com>
2016-09-08 00:26:09 -0700

    AA-005: add sleep 600 seconds for golang test

--- main_test.go
+++ main_test.go
@@ -5,8 +5,8 @@ import "fmt"
 
 func TestSomething(t *testing.T) {
 	fmt.Println(logo)
-        fmt.Println("sleep 20 seconds")
-	for i := 0; i < 20; i++ {
+        fmt.Println("sleep 600 seconds")
+	for i := 0; i < 600; i++ {
 		time.Sleep(time.Second)
 		fmt.Printf("slept %v seconds\n", i)
 	}
